bca4385d351a5c4ca6b2daffd55fa162651f6367
Jonathan Warner <jewarner57@gmail.com>
2020-09-01 17:27:22 -0400

    refactored get_turn_degrees_test

--- test_app.py
+++ test_app.py
@@ -4,7 +4,7 @@ from app import Flower
 def test_get_turn_degrees():
     """Tests if get turn degrees gets the number of degrees needed to rotate for each petal"""
     flower = Flower(10, "red", 0, 0, 0, 0)
-    flower2 = Flower(100, "red", 0, 0, 0, 0)
 
     assert flower.get_turn_degrees(10) == 36
-    assert flower2.get_turn_degrees(100) == 3.6
+    assert flower.get_turn_degrees(100) == 3.6
+    assert flower.get_turn_degrees(45) == 8
